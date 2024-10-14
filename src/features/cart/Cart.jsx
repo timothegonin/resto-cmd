4d4e1c1d3adfb971cf0eca1f748c4ba2d5b4ac76
timotheG <gonin.timothe@gmail.com>
2024-10-14 10:11:39 +0200

    ✨ modifications from a component

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useStore } from 'react-redux'
+import { SuperCremeux } from '../../common/models'
 
 const Cart = () => {
   const store = useStore()
+  const [list, setList] = useState(store.getState().list)
+
+  useEffect(() => {
+    store.subscribe(()=> setList(store.getState().list))
+  })
+
   return (
     <div className='Selection'>
       <h1>Vos produits sélectionnés</h1>
       {
-        store.getState().list.map((item, index) => {
+        list.map((item, index) => {
           return <span key={index} className='SelectedProduct'>{item.title} {item.price}</span>
         })
       }
+      <div className='CartNavBar'>
+        <button onClick={() => store.dispatch({type: 'ADD_PRODUCT', payload: SuperCremeux})}>Ajouter un super crémeux</button>
+      </div>
     </div>
   )
 }
