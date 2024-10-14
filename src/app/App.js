3e2e77b803f5278d77a9e88abbb3f06c9f700acf
timotheG <gonin.timothe@gmail.com>
2024-10-14 09:14:06 +0200

    🔧 configure store and app+good practices

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import { Provider } from 'react-redux';
+import './App.css';
+import {store} from './store'
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className='App'></div>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/common/models.js
@@ -0,0 +1,12 @@
+export const DoubleCantal = {
+  title: "Double Cantal",
+  price: 15.99,
+}
+export const SuperCremeux = {
+  title: "Super Cremeux",
+  price: 14.99,
+}
+export const PouletCroquant = {
+  title: "Poulet Croquant",
+  price: 17.99,
+}
