d3975fb1dcdc6026f219b5f8666e21b01dd9c89f
timotheG <gonin.timothe@gmail.com>
2024-10-16 15:19:55 +0200

    ♻️ improving totalOrder

--- src/app/selectors.js
+++ src/app/selectors.js
@@ -1,5 +1,5 @@
 export const getProductList = (state) => state?.list
 
-export const getTotalOrder = (state) => getProductList(state).reduce((prv, cur) => cur.price + prv, 0)
+export const getTotalOrder = (state) => getProductList(state).reduce((prv, cur) => Math.round((cur.price + prv) * 100) / 100, 0)
 
 export const isVoucherAvailable = (state) => getProductList(state).find((product) => product.title === "Super Crémeux")
